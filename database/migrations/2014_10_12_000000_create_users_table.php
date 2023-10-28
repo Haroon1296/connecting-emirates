778017b543667255eb140e8166feda2e514efa75
Haroon <haroon1296>
2023-10-28 06:03:06 -0700

    project setup ..

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,18 +13,45 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name')->nullable();
-            $table->string('last_name')->nullable();
+            $table->string('full_name')->nullable();
             $table->string('email')->unique()->nullable();
-            $table->enum('user_type', ['admin','guest','user','hat']);
+            $table->enum('user_type', ['admin','business','customer']);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('profile_image')->nullable();
             $table->string('phone_number')->nullable();
-            $table->string('zip_code')->nullable();
-            $table->longText('bio')->nullable();
-            $table->string('specialty')->nullable();
+            $table->string('restaurant_name')->nullable();
+            
+
+            $table->string('country_code')->nullable();
+            $table->longText('about')->nullable();
+            $table->date('date_of_birth')->nullable();
+
+            $table->enum('registering_as', ['restaurant', 'adventure', 'event'])->nullable();
+            $table->string('operting_emirates')->nullable();
+            $table->integer('category_id')->nullable();
+            $table->string('venue_seating')->nullable();
+            $table->string('dietary')->nullable();
+            $table->string('dietary')->nullable();
+            $table->string('menu_image')->nullable();
+            $table->string('license_image')->nullable();
+            // Business hours []
+            // Venue type []
+
+
+
+            $table->enum('gender', ['male', 'female', 'other'])->nullable();
+            $table->string('emirates')->nullable();
+            $table->string('nationality')->nullable();
+
+            $table->string('location')->nullable();
+            $table->string('latitude')->nullable();
+            $table->string('longitude')->nullable();
+
+
             $table->string('customer_id')->nullable();
+
             $table->rememberToken()->nullable();
+
             $table->enum('is_profile_complete', ['0','1'])->default('0');
             $table->enum('device_type', ['ios','android','web'])->nullable();
             $table->longText('device_token')->nullable();
@@ -35,6 +62,7 @@ return new class extends Migration
             $table->enum('is_social', ['0','1'])->default('0');
             $table->integer('verified_code')->nullable();
             $table->enum('is_blocked', ['0','1'])->default('0');
+
             $table->timestamps();
         });
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            ContentSeeder::class,
-            EventTypeSeeder::class,
+            ContentSeeder::class
         ]);
         // \App\Models\User::factory(10)->create();
 
